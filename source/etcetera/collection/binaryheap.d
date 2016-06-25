f1556f4528d6c61ea3fca154c056899fadf66c94
Gary Willoughby <dev@nomad.so>
2016-06-25 09:29:37 +0100

    Converted binary heap collection to use reference counting.

--- source/etcetera/collection/binaryheap.d
+++ source/etcetera/collection/binaryheap.d
@@ -9,9 +9,10 @@ module etcetera.collection.binaryheap;
 /**
  * Imports.
  */
+import core.exception;
 import core.memory;
+import core.stdc.stdlib : malloc, calloc, realloc, free;
 import core.stdc.string : memset;
-import etcetera.meta;
 import std.algorithm;
 import std.functional;
 import std.range;
@@ -26,8 +27,16 @@ import std.traits;
  *     than the second. This predicate defines the sorting order between the
  *     heap items and is called during insertion and extraction.
  */
-class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) == bool))
+struct BinaryHeap(T, alias pred) if (is(T == Unqual!T) && is(typeof(binaryFun!(pred)(T.init, T.init)) == bool))
 {
+	@nogc:
+	nothrow:
+
+	/**
+	 * The reference count.
+	 */
+	private int* _refCount;
+
 	/**
 	 * The comparison method.
 	 */
@@ -46,7 +55,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	/**
 	 * The minimum size in bytes that the heap will allocate.
 	 */
-	private immutable size_t _minSize;
+	private size_t _minSize;
 
 	/**
 	 * The current size in bytes of the heap.
@@ -63,13 +72,18 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 */
 	private bool _stateIsSorted;
 
+	/*
+	 * Disable the default constructor.
+	 */
+	@disable this();
+
 	/**
 	 * Construct a new binary heap.
 	 *
-	 * By default the heap is allocated enough memory for 8,192 items. If more
-	 * items are added, the heap can grow by doubling its allocation, ad
-	 * infinitum. If the items within reduce to only use half of the current
-	 * allocation the heap will half it. The heap will never shrink below the
+	 * When created, this collection is allocated enough memory for a minimum
+	 * amount of items. If the collection becomes full, the allocation will
+	 * double, ad infinitum. If items only occupy half of the collection, the
+	 * allocation will be halfed. The collection will never shrink below the
 	 * minimum capacity amount.
 	 *
 	 * Params:
@@ -82,23 +96,52 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *         $(PARAM_ROW OutOfMemoryError, If memory allocation fails.)
 	 *     )
 	 */
-	final public this(size_t minCapacity = 8_192) nothrow
+	public this(size_t minCapacity)
 	{
 		assert(minCapacity >= 1, "Heap must allow for at least one item.");
 
+		this._refCount  = cast(int*) malloc(int.sizeof);
+		*this._refCount = 1;
+
 		this._minSize = minCapacity * T.sizeof;
 		this._size    = this._minSize;
+		this._data    = cast(T*) calloc(minCapacity, T.sizeof);
+
+		if (this._data is null)
+		{
+			onOutOfMemoryError();
+		}
 
 		static if (hasIndirections!(T))
 		{
-			this._data = cast(T*)GC.calloc(this._size, GC.BlkAttr.NO_MOVE, typeid(T));
-		}
-		else
-		{
-			this._data = cast(T*)GC.calloc(this._size, GC.BlkAttr.NO_MOVE | GC.BlkAttr.NO_SCAN, typeid(T));
+			GC.addRange(this._data, this._size, typeid(T));
 		}
 
 		this._end = this._data - 1;
+	}
+
+	/**
+	 * Copy constructor post blit.
+	 */
+	public this(this) pure
+	{
+		*this._refCount += 1;
+	}
+
+	/**
+	 * Destructor.
+	 */
+	public ~this()
+	{
+		*this._refCount -= 1;
+
+		if (*this._refCount <= 0)
+		{
+			GC.removeRange(this._data);
+
+			free(this._refCount);
+			free(this._data);
+		}
 	}
 
 	/**
@@ -107,7 +150,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * Returns:
 	 *     The number of items stored in the heap.
 	 */
-	final public @property size_t count() const nothrow pure
+	public @property size_t count() const pure
 	{
 		return this._count;
 	}
@@ -118,7 +161,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * Returns:
 	 *     true if the heap is empty, false if not.
 	 */
-	final public @property bool empty() const nothrow pure
+	public @property bool empty() const pure
 	{
 		return (this._count == 0);
 	}
@@ -130,7 +173,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * Returns:
 	 *     The capacity of how many items the heap can hold.
 	 */
-	final private @property size_t capacity() const nothrow pure
+	private @property size_t capacity() const pure
 	{
 		return this._size / T.sizeof;
 	}
@@ -149,15 +192,28 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *         $(PARAM_ROW OutOfMemoryError, If memory reallocation fails.)
 	 *     )
 	 */
-	final public void insert(T item) nothrow
+	public void insert(T item)
 	{
 		this._end++;
 
 		if (this._count == this.capacity)
 		{
+			GC.removeRange(this._data);
+
 			this._size *= 2;
-			this._data  = cast(T*)GC.realloc(this._data, this._size, GC.BlkAttr.NONE, typeid(T));
-			this._end   = this._data + this._count;
+			this._data  = cast(T*) realloc(this._data, this._size);
+
+			if (this._data is null)
+			{
+				onOutOfMemoryError();
+			}
+
+			static if (hasIndirections!(T))
+			{
+				GC.addRange(this._data, this._size, typeid(T));
+			}
+
+			this._end = this._data + this._count;
 
 			memset(this._end, 0, this._size / 2);
 		}
@@ -180,7 +236,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *         $(PARAM_ROW AssertError, If the heap is empty.)
 	 *     )
 	 */
-	final public T peek() nothrow pure
+	public T peek() pure
 	{
 		assert(this._count, "Heap empty, peeking failed.");
 
@@ -200,21 +256,32 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *         $(PARAM_ROW OutOfMemoryError, If memory reallocation fails.)
 	 *     )
 	 */
-	final public T extract() nothrow
+	public T extract()
 	{
 		assert(this._count, "Heap empty, extracting failed.");
 
-		static T extracted;
-
 		this._count--;
-		extracted = *this._data;
+		auto extracted = *this._data;
 
 		*this._data = *this._end;
 
 		if ((this._count <= (this.capacity / 2)) && ((this._size / 2) >= this._minSize))
 		{
+			GC.removeRange(this._data);
+
 			this._size /= 2;
-			this._data  = cast(T*)GC.realloc(this._data, this._size, GC.BlkAttr.NONE, typeid(T));
+			this._data  = cast(T*) realloc(this._data, this._size);
+
+			if (this._data is null)
+			{
+				onOutOfMemoryError();
+			}
+
+			static if (hasIndirections!(T))
+			{
+				GC.addRange(this._data, this._size, typeid(T));
+			}
+
 			this._end   = this._data + (this._count - 1);
 		}
 		else
@@ -241,15 +308,31 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * Returns:
 	 *     true if the item is found on the heap, false if not.
 	 */
-	final public bool contains(T item)
+	public bool contains(T item) pure
 	{
-		for (T* x = this._data; x < this._data + this._count ; x++)
+		if (!this.empty)
 		{
-			if (*x == item)
+			for (T* x = this._data; x < this._data + this._count ; x++)
 			{
-				return true;
+				// For the time being we have to handle classes and interfaces as a
+				// special case when comparing because Object.opEquals is not @nogc.
+				static if (is(T == class) || is(T == interface))
+				{
+					if (*x is item)
+					{
+						return true;
+					}
+				}
+				else
+				{
+					if (*x == item)
+					{
+						return true;
+					}
+				}
 			}
 		}
+
 		return false;
 	}
 
@@ -264,12 +347,24 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *         $(PARAM_ROW OutOfMemoryError, If memory reallocation fails.)
 	 *     )
 	 */
-	final public void clear() nothrow
+	public void clear()
 	{
 		if (this._size > this._minSize)
 		{
+			GC.removeRange(this._data);
+
 			this._size = this._minSize;
-			this._data = cast(T*)GC.realloc(this._data, this._size, GC.BlkAttr.NONE, typeid(T));
+			this._data = cast(T*) realloc(this._data, this._size);
+
+			if (this._data is null)
+			{
+				onOutOfMemoryError();
+			}
+
+			static if (hasIndirections!(T))
+			{
+				GC.addRange(this._data, this._size, typeid(T));
+			}
 		}
 
 		memset(this._data, 0, this._size);
@@ -287,11 +382,11 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *     childIndex = The index of the item to sift up. The index must
 	 *     contain a child item.
 	 */
-	final private void siftUp(size_t childIndex) nothrow
+	private void siftUp(size_t childIndex)
 	{
-		static T parent;
-		static T child;
-		static size_t parentIndex;
+		T parent;
+		T child;
+		size_t parentIndex;
 
 		if (childIndex > 0)
 		{
@@ -335,13 +430,13 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *     parentIndex = The index of the item to sift down. The index must
 	 *     contain a parent item.
 	 */
-	final private void siftDown(size_t parentIndex) nothrow
+	private void siftDown(size_t parentIndex)
 	{
-		static T parent;
-		static T child1;
-		static T child2;
-		static size_t child1Index;
-		static size_t child2Index;
+		T parent;
+		T child1;
+		T child2;
+		size_t child1Index;
+		size_t child2Index;
 
 		child1Index = (2 * parentIndex) + 1;
 		child2Index = (2 * parentIndex) + 2;
@@ -403,7 +498,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * Even though we are tinkering with the internal state here, once sorted
 	 * it's still a fully correct heap.
 	 */
-	final private auto sort() nothrow
+	private auto sort()
 	{
 		if (!this._stateIsSorted)
 		{
@@ -422,7 +517,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * ---
 	 * import std.algorithm;
 	 *
-	 * auto heap = new BinaryHeap!(int, "a > b");
+	 * auto heap = BinaryHeap!(int, "a > b")(16);
 	 *
 	 * heap.insert(2);
 	 * heap.insert(1);
@@ -437,7 +532,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 *     performance cost of sorting the internal state before the range is
 	 *     returned.
 	 */
-	final public auto byValue() nothrow
+	public auto byValue()
 	{
 		static struct Result
 		{
@@ -495,7 +590,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * ---
 	 * import std.stdio;
 	 *
-	 * auto heap = new BinaryHeap!(int, "a < b");
+	 * auto heap = BinaryHeap!(int, "a < b")(16);
 	 * heap.insert(2);
 	 * heap.insert(1);
 	 * heap.insert(3);
@@ -506,7 +601,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * }
 	 * ---
 	 */
-	final public int opApply(ForeachAggregate!(T) dg) nothrow
+	public int opApply(scope int delegate(ref T) nothrow @nogc dg)
 	{
 		int result;
 		this.sort();
@@ -540,7 +635,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * ---
 	 * import std.stdio;
 	 *
-	 * auto heap = new BinaryHeap!(int, "a < b");
+	 * auto heap = BinaryHeap!(int, "a < b")(16);
 	 * heap.insert(2);
 	 * heap.insert(1);
 	 * heap.insert(3);
@@ -551,7 +646,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 	 * }
 	 * ---
 	 */
-	final public int opApply(IndexedForeachAggregate!(T) dg) nothrow
+	public int opApply(scope int delegate(ref size_t, ref T) nothrow @nogc dg)
 	{
 		int result;
 		size_t index;
@@ -574,7 +669,7 @@ class BinaryHeap(T, alias pred) if (is(typeof(binaryFun!(pred)(T.init, T.init)) 
 ///
 unittest
 {
-	auto heap = new BinaryHeap!(int, "a > b");
+	auto heap = BinaryHeap!(int, "a > b")(8);
 	heap.insert(1);
 	heap.insert(3);
 	heap.insert(2);
@@ -594,11 +689,55 @@ unittest
 	assert(heap.count == 0);
 }
 
+// Test reference counting.
+
+unittest
+{
+	auto foo(T)(T heap)
+	{
+		assert(*heap._refCount == 2);
+	}
+
+	auto bar(T)(ref T heap)
+	{
+		assert(*heap._refCount == 1);
+	}
+
+	auto baz(T)(T heap)
+	{
+		assert(*heap._refCount == 1);
+		return heap;
+	}
+
+	auto qux()
+	{
+		return BinaryHeap!(string, "a > b")(1);
+	}
+
+	auto heap = BinaryHeap!(string, "a > b")(16);
+
+	assert(*heap._refCount == 1);
+
+	foo(heap);
+	assert(*heap._refCount == 1);
+
+	bar(heap);
+	assert(*heap._refCount == 1);
+
+	heap = baz(BinaryHeap!(string, "a > b")(1));
+	assert(*heap._refCount == 1);
+
+	heap = qux();
+	assert(*heap._refCount == 1);
+}
+
+// Test big datasets.
+
 unittest
 {
 	import std.algorithm;
 
-	auto heap = new BinaryHeap!(int, (int a, int b) => a > b);
+	auto heap = BinaryHeap!(int, (int a, int b) => a > b)(8_192);
 
 	assert(heap.empty);
 	assert(heap.count == 0);
@@ -652,9 +791,11 @@ unittest
 	assert(heap.capacity == 8_192);
 }
 
+// Test the memory layout.
+
 unittest
 {
-	auto heap = new BinaryHeap!(int, delegate(int a, int b){return a > b;})(4);
+	auto heap = BinaryHeap!(int, delegate(int a, int b){return a > b;})(4);
 
 	assert(heap.empty);
 	assert(heap.count == 0);
@@ -715,6 +856,8 @@ unittest
 	assert(heap.count == 0);
 }
 
+// Test storing objects.
+
 unittest
 {
 	class Person
@@ -722,25 +865,48 @@ unittest
 		public int priority;
 		public string name;
 
-		public this(int priority, string name) nothrow
+		public this(int priority, string name)
 		{
 			this.priority = priority;
 			this.name     = name;
 		}
 	}
 
-	auto priorityQueue = new BinaryHeap!(Person, "a.priority > b.priority");
+	auto priorityQueue = BinaryHeap!(Person, "a.priority > b.priority")(1);
+	auto foo           = new Person(1, "Foo");
 
-	priorityQueue.insert(new Person(1, "Foo"));
+	priorityQueue.insert(foo);
 	priorityQueue.insert(new Person(4, "Bar"));
 	priorityQueue.insert(new Person(2, "Baz"));
-	priorityQueue.insert(new Person(3, "Quxx"));
 
+	assert(priorityQueue.contains(foo));
+	assert(!priorityQueue.contains(new Person(1, "Foo")));
 	assert(priorityQueue.extract().name == "Bar");
-	assert(priorityQueue.extract().name == "Quxx");
-	assert(priorityQueue.extract().name == "Baz");
-	assert(priorityQueue.extract().name == "Foo");
+
+	priorityQueue.clear();
+	assert(priorityQueue.empty);
 }
+
+// Test storing structs.
+
+unittest
+{
+	struct Foo
+	{
+		public int foo;
+	}
+
+	auto heap = BinaryHeap!(Foo, "a.foo > b.foo")(16);
+	auto foo  = Foo(1);
+
+	heap.insert(foo);
+
+	assert(heap.contains(foo));
+	assert(!heap.contains(Foo(2)));
+	assert(heap.extract().foo == 1);
+}
+
+// Test the range interface.
 
 unittest
 {
@@ -748,7 +914,7 @@ unittest
 
 	int limit = 1_000;
 
-	auto heap = new BinaryHeap!(int, "a > b");
+	auto heap = BinaryHeap!(int, "a > b")(16);
 
 	for (int x = 1; x <= limit ; x++)
 	{
@@ -757,6 +923,7 @@ unittest
 
 	assert(heap.byValue.canFind(500));
 	assert(heap.byValue.take(5).array == [1000, 999, 998, 997, 996]);
+	assert(heap.byValue.save.take(5).array == [1000, 999, 998, 997, 996]);
 
 	for (int x = limit; x >= 1 ; x--)
 	{
@@ -764,9 +931,11 @@ unittest
 	}
 }
 
+// Test iteration.
+
 unittest
 {
-	auto heap = new BinaryHeap!(int, "a < b");
+	auto heap = BinaryHeap!(int, "a < b")(16);
 	heap.insert(4);
 	heap.insert(1);
 	heap.insert(3);
@@ -787,22 +956,5 @@ unittest
 		assert(index == counter);
 		assert(value == data[counter++]);
 	}
-
-	counter = 0;
-	foreach (value; heap.byValue)
-	{
-		assert(value == data[counter++]);
-	}
-
-	counter = 0;
-	foreach (value; heap.byValue.save)
-	{
-		assert(value == data[counter++]);
-	}
-
-	assert(heap.extract() == 1);
-	assert(heap.extract() == 2);
-	assert(heap.extract() == 3);
-	assert(heap.extract() == 4);
 }
 
