987c0592c6a4ca05e6339d8d77e074bd1f450b1f
Gary Willoughby <dev@nomad.so>
2016-06-26 17:23:08 +0100

    Removed hash map collection to be rewritten.

--- source/etcetera/collection/package.d
+++ source/etcetera/collection/package.d
@@ -8,7 +8,6 @@
  *     $(P
  *         $(PARAM_TABLE
  *             $(PARAM_ROW $(LINK2 ./binaryheap.html, BinaryHeap), A generic binary heap implementation.)
- *             $(PARAM_ROW $(LINK2 ./hashmap.html, HashMap), A generic hash map implementation.)
  *             $(PARAM_ROW $(LINK2 ./linkedlist.html, LinkedList), A generic doubly linked list implementation.)
  *             $(PARAM_ROW $(LINK2 ./queue.html, Queue), A generic first-in-first-out (FIFO) queue implementation.)
  *             $(PARAM_ROW $(LINK2 ./stack.html, Stack), A generic last-in-first-out (LIFO) stack implementation.)
@@ -18,7 +17,6 @@
 module etcetera.collection;
 
 public import etcetera.collection.binaryheap;
-public import etcetera.collection.hashmap;
 public import etcetera.collection.linkedlist;
 public import etcetera.collection.queue;
 public import etcetera.collection.stack;
